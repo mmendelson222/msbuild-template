bcffa875eaeee94518708566563867774b8297e4
Michael Mendelson <mmendelson@gmail.com>
2015-11-11 11:32:37 -0500

    initial commit

---
+++ CodeDeployTasks/HelloTask.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+
+namespace CodeDeployTasks
+{
+    /// <summary>
+    /// Execute a custom tasks that will log a simple message
+    /// </summary>
+    public class HelloTask : Task
+    {
+        // Optional property
+        public string Prop { get; set; }
+
+        // Required property indicated by Required attribute
+        [Required]
+        public string RequiredProp { get; set; }
+
+        /// <summary>
+        /// This method is called automatically when the task is run.
+        /// </summary>
+        /// <returns>Boolean to indicate if the task was sucessful.</returns>
+        public override bool Execute()
+        {
+            Log.LogCommandLine(Prop);
+            Log.LogCommandLine(RequiredProp);
+            return true;
+        }
+    }
+}
